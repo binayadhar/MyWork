7a36a00a2ff620c8226fa7e5c5b4f100094358bc
binayadhar <54002195+binayadhar@users.noreply.github.com>
2019-08-11 22:13:44 +0530

    Add files via upload

---
+++ Releaf.bat
@@ -0,0 +1,65 @@
+@echo off
+color 2
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Movie.bat virus remover program, Written by Binaya!!!!!!!!!!!!!
+echo.
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Please close all working windows and let the program run!!!!!!!!!!!!!!!
+echo.
+echo !!!!------!!!!---------!!!-----------!!!Press enter to continue-------!!!------------!!!-----------!!!-
+echo.
+echo.
+pause
+
+del "%appdata%\Microsoft\windows\start menu\Programs\Startup\system.bat"
+del "%appdata%\Microsoft\windows\start menu\Programs\Startup\windows.vbs"
+del "%appdata%\movie.bat d:
+del "%appdata%\movie.bat e:
+del "%appdata%\movie.bat f:
+del "%appdata%\movie.bat g:
+del "%appdata%\movie.bat h:
+del "%appdata%\movie.bat i:
+del "%appdata%\movie.bat j:
+del "%appdata%\movie.bat k:
+del "%appdata%\movie.bat l:
+del "%appdata%\movie.bat m:
+del "%appdata%\movie.bat n:
+del "%appdata%\movie.bat o:
+del "%appdata%\movie.bat p:
+del "%appdata%\movie.bat q:
+del "%appdata%\movie.bat r:
+del "%appdata%\movie.bat s:
+del "%appdata%\movie.bat t:
+del "%appdata%\movie.bat u:
+del "%appdata%\vol.bat"
+del "%appdata%\movie.bat"
+cls
+color 9f
+cd c:/
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!   Please Wait, deep search 1 is running !!! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+echo !!!!!!!!!!!!!!!!!!!! Notice, if the output is " FILE NOT FOUND " Then its working!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+dir movie.bat /s
+Pause
+cls
+color f4
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!   Please Wait, deep search 2 is running !!! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+echo !!!!!!!!!!!!!!!!!!!! Notice, if the output is " FILE NOT FOUND " Then its working!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+dir system.bat /s
+Pause
+cls
+color 4f
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!   Please Wait, deep search 3 is running !!! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+echo !!!!!!!!!!!!!!!!!!!! Notice, if the output is " FILE NOT FOUND " Then its working!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+dir system.vbs /s
+pause
+cls
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!                                          !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+echo !!!!!!!!!!!!!!!!!!!! 						             !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+echo !!!!!!!!!!!!!!!!!!!! System is cleaned but be sure before puting infected drives   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+echo !!!!!!!!!!!!!!!!!!!! 						             !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!             We will exit now             !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+pause
+
